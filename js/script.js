0af800bfa057e342fd7ba6de649a6f3733729e24
Martin Delgado <martindelgado2011@gmail.com>
2021-12-24 18:18:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
-// Spoonacular api key: da2c4227946040d386d435fdc069b601
-const spoonacularkey = "?apiKey=da2c4227946040d386d435fdc069b601"
-var askPropertiesRecipe = {};
-var askIngredientsRecipe = {};
-var askRandomRecipe = "https://api.spoonacular.com/recipes/random";
+const SPOONACULAR_KEY = "?apiKey=da2c4227946040d386d435fdc069b601"
+const ASK_PROPERTIES_RECIPE = "https://api.spoonacular.com/recipes/findByNutrients";
+const ASK_INGREDIENTS_RECIPE = "https://api.spoonacular.com/recipes/findByIngredients"; //params: &ingredients, &ignorePantry=true
+const ASK_RANDOM_RECIPE = "https://api.spoonacular.com/recipes/random"; //params: &intolerances, &diet, &meal type (&type)
+
 var recipe = {};
 
-async function getRecipe(){
-    let response = await fetch(askRandomRecipe+spoonacularkey);
-    recipe = await response.json();
-    console.log(recipe);
+var reqIntolerances = [];
+var reqDiet = [];
+var reqType = [];
+var reqParams = "&intolerances="+reqIntolerances+"&diet="+reqDiet+"&type="+reqType;
+
+var form = document.getElementById("random__form");
+    inputs = form.getElementsByTagName
+
+
+
+
+function getParameters(){
+    reqIntolerances.push(document.getElementById("testintoler").value);
 };
 
-//document.getElementById("BTNrandomRecipe").addEventListener("click", getRecipe());
+
+async function getRecipe(reqRecipe, reqParameters){
+    let response = await fetch(reqRecipe+SPOONACULAR_KEY+reqParameters);
+    recipe = await response.json();
+    console.log(recipe);
+
+    var data = document.getElementById("recipe__description");
+
+    for (let i = 0; i < recipe.length; i++) {
+        const element = recipe[i];
+
+        var div = document.createElement("div");
+        div.innerHTML = recipe[i];
+        data.appendChild(div);
+    }
+
+};
+
+/*document.getElementById("BTNrandomRecipe").addEventListener("click", getRecipe());
+
+? number=1 & tags=vegetarian,dessert
+
+? intolerances=  {} , {}  , {}  &  tags= {} , {}
+
+*/
