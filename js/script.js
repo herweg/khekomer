27c387c675a01b1a385ecf5261e8b4263bca943e
Martin Delgado <martindelgado2011@gmail.com>
2021-12-24 01:14:13 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
-// // Spoonacular api key: da2c4227946040d386d435fdc069b601
-// const spoonacularkey = "?apiKey=da2c4227946040d386d435fdc069b601"
-// var askPropertiesRecipe = {};
-// var askIngredientsRecipe = {};
-// var askRandomRecipe = "https://api.spoonacular.com/recipes/random";
+// Spoonacular api key: da2c4227946040d386d435fdc069b601
+const spoonacularkey = "?apiKey=da2c4227946040d386d435fdc069b601"
+var askPropertiesRecipe = {};
+var askIngredientsRecipe = {};
+var askRandomRecipe = "https://api.spoonacular.com/recipes/random";
+var recipe = {};
 
-// function getRecipe(){
-//     let response = await fetch(askRandomRecipe+spoonacularkey);
-//     let commits = await response.json();
-//     console.log(commits);
-//     console.log("test");
-// };
+async function getRecipe(){
+    let response = await fetch(askRandomRecipe+spoonacularkey);
+    recipe = await response.json();
+    console.log(recipe);
+};
 
 //document.getElementById("BTNrandomRecipe").addEventListener("click", getRecipe());
-
-function getRecipe(){
-    fetch("https://api.spoonacular.com/recipes/random?apiKey=da2c4227946040d386d435fdc069b601")
-    .then(response => response.json())
-    .then(recipes => console.log(recipes));
-}
